b6df52c792820246ad995f5c20ded966c90b7517
Sanzio Monti <github@sanziomonti.xyz>
2022-05-16 00:18:35 +0200

    home - clean file

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ScrollToTop from "../components/scroll-to-top";
 import SEO from "../components/seo";
-import AboutService from "../containers/about/about-service/index.jsx";
 import ProjectsContainer from "../containers/home/projects";
 import Movies from "../containers/movies";
-import PageTitleContainer from "../containers/global/page-title";
 import Footer from "../layouts/footer";
 import Header from "../layouts/header";
 import Layout from "../layouts";
@@ -17,14 +15,8 @@ const HomePage = () => {
                 <div className="wrapper about-page-wrapper">
                     <Header classOption="container" />
                     <div className="main-content">
-                        {/* <PageTitleContainer /> */}
-                        {/* <AboutService /> */}
                         <ProjectsContainer />
-                        {/* <Blockquote /> */}
-                        {/* <AboutGallery /> */}
                         <Movies />
-                        {/*  <AboutAward /> */}
-                        {/* <BrandContainer /> */}
                     </div>
                     <Footer />
                     <ScrollToTop />
