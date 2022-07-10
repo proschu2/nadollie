4c33112b4405d9ab3031bc66bdefe3bce50f8b23
Sanzio Monti <github@sanziomonti.xyz>
2022-07-10 23:30:58 +0200

    footer

--- src/layouts/footer/index.jsx
+++ src/layouts/footer/index.jsx
@@ -2,97 +2,50 @@ import { Link } from "react-router-dom";
 import FooterLogo from "../../components/footer-logo";
 
 const Footer = () => {
-    return null;
     return (
-        <footer className="footer-area reveal-footer border-top-style">
+        <footer className=" footer-area reveal-footer container">
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-sm-12">
                         <div className="footer-content">
                             <div className="widget-item">
-                                <div className="widget-footer-nav">
-                                    <nav>
-                                        <ul>
-                                            <li>
-                                                <Link
-                                                    to={
-                                                        process.env.PUBLIC_URL +
-                                                        "/"
-                                                    }
-                                                >
-                                                    term &amp; condition
-                                                </Link>
-                                            </li>
-                                            <li>
-                                                <Link
-                                                    to={
-                                                        process.env.PUBLIC_URL +
-                                                        "/"
-                                                    }
-                                                >
-                                                    policy
-                                                </Link>
-                                            </li>
-                                            <li>
-                                                <Link
-                                                    to={
-                                                        process.env.PUBLIC_URL +
-                                                        "/"
-                                                    }
-                                                >
-                                                    map
-                                                </Link>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                </div>
+                                nadina dollie (1995) is a swiss based
+                                cinematographer
                             </div>
-                            <div className="widget-item text-center">
-                                <div className="about-widget">
-                                    <FooterLogo
-                                        image={`${process.env.PUBLIC_URL}/img/logo.png`}
-                                    />
-                                </div>
+                            {/* <div className="widget-item">
+                                <div className="widget-footer-nav"></div>
+                            </div> */}
+                            <div className="widget-item">
                                 <div className="widget-copyright">
                                     <p>
-                                        © 2021 <span>Alexis</span>. Made with{" "}
-                                        <i className="icofont-heart-alt"></i> by{" "}
-                                        <a
-                                            target="_blank"
-                                            href="https://www.hasthemes.com"
-                                            rel="noreferrer"
-                                        >
-                                            HasThemes
-                                        </a>
+                                        © {new Date().getFullYear()} all rights
+                                        reserved
                                     </p>
                                 </div>
                             </div>
                             <div className="widget-item">
                                 <ul className="widget-social">
-                                    <li className="social-text">
-                                        <span>follow us on social</span>
-                                    </li>
                                     <li>
                                         <a
-                                            href="https://twitter.com/"
+                                            href="tel:+41794226696"
                                             target="_blank"
                                             rel="noopener noreferrer"
                                         >
-                                            <i className="social_twitter"></i>
+                                            <i className="icon_phone"></i>
                                         </a>
                                     </li>
                                     <li>
                                         <a
-                                            href="https://www.facebook.com/"
+                                            href="mailto:film@nadinadollie.com"
                                             target="_blank"
                                             rel="noopener noreferrer"
                                         >
-                                            <i className="social_facebook"></i>
+                                            <i className="icon_mail"></i>
                                         </a>
                                     </li>
                                     <li>
                                         <a
-                                            href="https://www.instagram.com/"
+                                            href="https://www.instagram.com/pashmina__/"
                                             target="_blank"
                                             rel="noopener noreferrer"
                                         >
